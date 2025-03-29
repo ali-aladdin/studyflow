222607360b54440ad04eb8abfe151598f27fed43
Abscertain <alibassamaladdin@gmail.com>
2025-03-29 16:50:13 +0200

    Add search bars for notes and flashcards

--- lib/pages/flashcards_screen.dart
+++ lib/pages/flashcards_screen.dart
@@ -9,16 +9,48 @@ class FlashcardsScreen extends StatefulWidget {
 }
 
 class _FlashcardsScreenState extends State<FlashcardsScreen> {
+  final TextEditingController _searchText = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
+        title: Text('Flashcards'),
         backgroundColor: AppColors.secondaryColor,
-        title: const Text(
-          'Flashcards',
-          style: TextStyle(
-            color: AppColors.primaryColor,
+        foregroundColor: AppColors.primaryColor,
+        toolbarHeight: 60.0,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(45.0),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextFormField(
+              controller: _searchText,
+              decoration: const InputDecoration(
+                labelText: 'Search for flashcards',
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.textColor,
+                ),
+                suffixIcon: Icon(
+                  Icons.search,
+                  color: AppColors.textColor,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  borderSide: BorderSide(
+                    color: AppColors.primaryColor,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: AppColors.primaryColor,
+                  ),
+                ),
+                filled: true,
+                fillColor: AppColors.primaryColor,
+              ),
+            ),
           ),
         ),
       ),
--- lib/pages/notes_screen.dart
+++ lib/pages/notes_screen.dart
@@ -9,16 +9,48 @@ class NotesScreen extends StatefulWidget {
 }
 
 class _NotesScreenState extends State<NotesScreen> {
+  final TextEditingController _searchText = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
+        title: Text('Notes'),
         backgroundColor: AppColors.secondaryColor,
-        title: const Text(
-          'Notes',
-          style: TextStyle(
-            color: AppColors.primaryColor,
+        foregroundColor: AppColors.primaryColor,
+        toolbarHeight: 60.0,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(45.0),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextFormField(
+              controller: _searchText,
+              decoration: const InputDecoration(
+                labelText: 'Search for notes',
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.textColor,
+                ),
+                suffixIcon: Icon(
+                  Icons.search,
+                  color: AppColors.textColor,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  borderSide: BorderSide(
+                    color: AppColors.primaryColor,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: AppColors.primaryColor,
+                  ),
+                ),
+                filled: true,
+                fillColor: AppColors.primaryColor,
+              ),
+            ),
           ),
         ),
       ),
